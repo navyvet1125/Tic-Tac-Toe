ab1d6ac89349cf6c53d0cdfebd89871881718ec3
Evan Washington <enavy04@gmail.com>
2016-03-13 09:39:07 -0700

    I got it to workgit add js/app.js !

--- js/app.js
+++ js/app.js
@@ -5,11 +5,28 @@ var gameBoard ={
 	playerWins:0,
 	computerWins:0,
 	tiedGames:0,
-	playerStart: false,
-	isPlayerX: true,
+	isPlayerX: false,
 	gameStart: false
 };
 
+var computerWin = function(){
+	gameBoard.gameStart =false;
+	gameBoard.computerWins++;
+	gameBoard.isPlayerX= false;
+	alert('I win!!!');
+}
+
+var playerWin = function(){
+	gameBoard.gameStart =false;
+	gameBoard.playerWins++;
+	gameBoard.isPlayerX= true;
+	alert('You win!!!');
+}
+
+var catsWin = function(){
+	alert('Cats Game!');
+	gameBoard.gameStart =false;
+}
 
 var isSpaceClear = function(x,y) {
 	return (gameBoard.board[x][y]===null);
@@ -111,7 +128,7 @@ var getWinner = function () {
 
 
 var playerChoice = function (element) {
-	console.log(gameBoard.gameStart && !isCatsGame() && !getWinner());
+	
 	if(gameBoard.gameStart && !isCatsGame() && !getWinner()) {
 		var elementIdString = element.id.toString();
 		var column = elementIdString[elementIdString.length-1];
@@ -127,52 +144,73 @@ var playerChoice = function (element) {
 				gameBoard.board[row][column]='O';
 			}
 			var isWinner = getWinner();
-			if(!isWinner){
+			var isCats = isCatsGame();
+			
+			if(!isWinner && !isCats){
 				computerChoice();
+				decideWinner(getWinner());
 			} else {
-				
+				decideWinner(getWinner());
 			}
 		}
 	}
+}
+
+
+var decideWinner = function (whoWon){
+	var isPlayerX = gameBoard.isPlayerX;
+	if (whoWon==='X') {
+		if(isPlayerX) {
+		 	playerWin();
+		} else {
+			computerWin();
+		}
+	} else if (whoWon==='O') {
+		if(!isPlayerX) {
+		 	playerWin();
+		} else {
+			computerWin();
+		}
+
+	} else if(isCatsGame()) {
+		catsWin();
+	}
+
 }
 
 var computerChoice = function(){
 	var row = Math.floor(Math.random()*3);
 	var column = Math.floor(Math.random()*3);
-	if (!isCatsGame()){
-
-		if (isSpaceClear(row, column)) {
-			var element = document.getElementById('box'+row+column);
-			if (gameBoard.isPlayerX) {
-				element.innerHTML ='<div class="selected-O"></div>';
-				gameBoard.board[row][column]='O';
-				
-			} else {
-				element.innerHTML ='<div class="selected-X"></div>';
-				gameBoard.board[row][column]='X';
-			}
+	if (isSpaceClear(row, column)) {
+		var element = document.getElementById('box'+row+column);
+		if (gameBoard.isPlayerX) {
+			element.innerHTML ='<div class="selected-O"></div>';
+			gameBoard.board[row][column]='O';
+			
 		} else {
-			computerChoice();
+			element.innerHTML ='<div class="selected-X"></div>';
+			gameBoard.board[row][column]='X';
 		}
-
+	} else {
+		computerChoice();
 	}
-
 }
 
 var startGame = function() {
 	clearBoard();
-	if(!gameBoard.playerStart) {
-		gameBoard.isPlayerX=false;
+	if(!gameBoard.isPlayerX) {
 		computerChoice();
 	}
 	gameBoard.gameStart= true;
 }
 
 var clearBoard = function () {
-	for (var x = 0; x<3; x++){
-		for (var y =0; y<3;y++){
-			gameBoard.board[x][y]=null;
-			document.getElementById('box'+x+y).innerHTML='';
+	if (!gameBoard.gameStart) {
+		for (var x = 0; x<3; x++){
+			for (var y =0; y<3;y++){
+				gameBoard.board[x][y]=null;
+				document.getElementById('box'+x+y).innerHTML='';
+			}
 		}
 	}
 
